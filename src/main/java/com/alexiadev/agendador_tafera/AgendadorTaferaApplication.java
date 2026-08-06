7dac579f7225b1cd480b5bedc6e637036bb65ce7
Alexia <alexiacastro2000@gmail.com>
2026-08-05 22:13:34 -0300

    commit inicial com esqueleto

---
+++ src/main/java/com/alexiadev/agendador_tafera/AgendadorTaferaApplication.java
@@ -0,0 +1,13 @@
+package com.alexiadev.agendador_tafera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendadorTaferaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendadorTaferaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexiadev/agendador_tafera/AgendadorTaferaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexiadev.agendador_tafera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorTaferaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
